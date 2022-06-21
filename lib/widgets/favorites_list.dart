a400cfde3a450850f7c4961de72d25dd9903ca8b
MK <zxdsl852@gmail.com>
2022-06-21 23:55:25 +0200

    added functionality of registering domain for bookmarked names

--- lib/widgets/favorites_list.dart
+++ lib/widgets/favorites_list.dart
@@ -1,7 +1,38 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
+import 'package:url_launcher/url_launcher.dart';
+import 'dart:core';
+
+Future<void> registerDomain(BuildContext context, String str) async{
+  str = str.toLowerCase().replaceAll(RegExp(r'\s+'), '');
+
+  return await showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text("Register domain"),
+        content: Text("Do you want to open external site to try register domain '$str'"),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              final uri = Uri.https('domains.google.com', '/registrar/search', {'searchTerm': str});
+              launchUrl(uri);
+              Navigator.of(context).pop(true);
+            },
+            child: const Text("Ok")
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text("Cancel"),
+          ),
+        ],
+      );
+    },
+  );
+}
 
 class FavoriteNames extends Cubit<List<String>>{
   late _PersistenceManager? _manager;
@@ -69,30 +100,58 @@ Widget favoritesListBuilder(BuildContext context) =>
         return list.isEmpty
             ? const Center(child: Text('Add something to favourite list'))
             : ReorderableListView.builder(
+                buildDefaultDragHandles: true,
                 itemCount: list.length,
                 onReorder: (fromIdx, toIdx){
                   context.read<FavoriteNames>().swap(fromIdx, toIdx);
                 },
                 itemBuilder: (context, index){
                   final text = list[index];
-                  return Dismissible(
-                    direction: DismissDirection.endToStart,
+                  return Padding(
                     key: ValueKey<String>(text),
-                    background: Container(
-                      color: Colors.red, 
-                      alignment: AlignmentDirectional.centerEnd,
-                      child: const Icon(Icons.delete),
-                    ),
-                    onDismissed: (DismissDirection _){
-                      context.read<FavoriteNames>().remove(text);
-                    },
-                    child: ListTile(
-                      dense: true,
-                      title: Text(text),
-                      trailing: TextButton(
-                        onPressed: ()=> context.read<FavoriteNames>().remove(text),
-                        child: const Icon(Icons.star_border),
-                      )
+                    padding: const EdgeInsets.only(right: 16.0),
+                    child: Dismissible(
+                      direction: DismissDirection.horizontal,
+                      key: ValueKey<String>(text),
+                      background: Container(
+                        color: Colors.green, 
+                        alignment: AlignmentDirectional.centerStart,
+                        child: const Icon(Icons.domain_add),
+                      ),
+                      secondaryBackground: Container(
+                        color: Colors.red, 
+                        alignment: AlignmentDirectional.centerEnd,
+                        child: const Icon(Icons.delete),
+                      ),
+                      onDismissed: (DismissDirection _){
+                        context.read<FavoriteNames>().remove(text);
+                      },
+                      confirmDismiss: (direction){
+                        if(direction == DismissDirection.endToStart) return Future.value(true);
+                        registerDomain(context, text);
+                        return Future.value(false);
+                      },
+                      child: ListTile(
+                        enableFeedback: true,
+                        enabled: true,
+                        dense: true,
+                        title: Text(text),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              iconSize: 25,
+                                icon: const Icon(Icons.copy),    
+                                onPressed: () => Clipboard.setData(ClipboardData(text: text)),    
+                              ),
+                            IconButton(
+                              iconSize: 25,
+                              onPressed: ()=> context.read<FavoriteNames>().remove(text),
+                              icon: const Icon(Icons.delete_outline),
+                            )
+                        ]
+                        )
+                      ),
                     ),
                   );
                 },
